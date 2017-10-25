7d25039ae28925a7d277000fce6a010c357a5d5f
David Pham <dppham1@uci.edu>
2017-10-25 01:31:21 -0700

    Update server.go

--- server/server.go
+++ server/server.go
@@ -6,45 +6,33 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"github.com/willf/bloom"
 )
 
-// *** Structs ***
+// *** GLOBAL VARIABLES ***
+
+var bloomFilter *bloom.BloomFilter
+
+// *** STRUCTS ***
 
 type User struct {
 	UserID int
 	Email  []string
 }
 
-type Payload struct {
-	UserData []User
+func createBloomFilter() *bloom.BloomFilter {
+	n := uint(1000)
+	filter := bloom.New(20*n, 5)
+	return filter
 }
 
-// **************
-
-// func serveRest(w http.ResponseWriter, r *http.Request) {
-// 	response, err := getJsonResponse()
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	fmt.Fprintf(w, string(response))
-// }
-
-func retrieve(w http.ResponseWriter, r *http.Request) {
-	//url := "http://localhost:8081"
-	// res, err := http.Get(url)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer res.Body.Close()
+func populateBF(w http.ResponseWriter, r *http.Request) {
 	body, err := ioutil.ReadAll(r.Body)
-
 	if err != nil {
 		panic(err)
 	}
 	
 	defer r.Body.Close()
-
 
 	var p []User
 
@@ -56,20 +44,21 @@ func retrieve(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.WriteHeader(http.StatusAccepted)
-	w.Write([]byte("success"))
+	w.Write([]byte("Success"))
 
-	// Make a loop here later
 	for i := 0; i < len(p); i++ {
-		fmt.Println("User ID: ", p[i].UserID)
-		fmt.Println("User Email: ", p[i].Email)
+		for j := 0; j < len(p[i].Email); j++ {
+			bloomFilter.Add([]byte(fmt.Sprintf("%d|%s", p[i].UserID, p[i].Email[j])))
+		}
 	}
 
-	// *** Can write back to Client here if we want
 }
 
+
 func main() {
-	//I have mine on 8082 because my 8081 Server wouldn't shut down
 	url := ":8082"
-	http.HandleFunc("/retrieve", retrieve)
+	bloomFilter = createBloomFilter()
+	http.HandleFunc("/populateBF", populateBF)
 	log.Fatal(http.ListenAndServe(url, nil))
+
 }

8c29fe4e06cee9a262ea7a020821504045071fd0
David Pham <dppham1@uci.edu>
2017-11-21 15:00:28 -0800

    extra benchmark tests

--- runClient.go
+++ runClient.go
@@ -18,15 +18,17 @@ func main() {
 	defer db.Close()
 
 	// populate the Bloom Filter from values in the MySQL Database
-	client.Populate()
+	// client.Populate()
 	
 	// to check if values are in the Bloom Filter. Note: Remember to replace the value of b and the userID in Check to what they are in the MySQL DB.
 	// b := []string{"NmNTsOQJOl@aol.com"}
 	// client.Check(5, b)
 
 	// drop all of the tables in UserStructs schema
-	err := client.DropTables(db)
-	if err != nil {
-		fmt.Println(err)
-	}
+	// err := client.DropTables(db)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	client.HealthCheck()
 }

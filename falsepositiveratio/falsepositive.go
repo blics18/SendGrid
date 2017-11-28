e681ffab8312aaf7dfde2dca8756914d59949229
BL MacBook <BLMac@Brendas-MacBook-Pro.local>
2017-11-27 17:40:03 -0800

    Response from check() changed to struct

--- falsepositiveratio/falsepositive.go
+++ falsepositiveratio/falsepositive.go
@@ -2,28 +2,30 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	// "strconv"
 
 	"github.com/blics18/SendGrid/client"
 )
 
 func testRatio() {
 	//CreateBloomFilter(size)
-	client.PopulateDB(10, 10, 5)
+	client.PopulateDB(10, 5000, 5)
 	client.Populate()
-	users := client.MakeRandomUsers(5, 100)
-	counter := 0
+	users := client.MakeRandomUsers(10, 5000)
+	numMisses := 0
 	totalEmails := 0
 	for i := range users {
-		totalEmails += len(users[i].Email)
+		// totalEmails += len(users[i].Email)
 		_, resp := client.Check(*users[i].UserID, users[i].Email)
-		for x := range resp {
-			y, _ := strconv.Atoi(string(resp[x]))
-			counter += y
-		}
+		numMisses += resp.Hits
+		totalEmails += resp.Total
+		// for x := range resp {
+		// 	y, _ := strconv.Atoi(string(resp[x]))
+		// 	counter += y
+		// }
 	}
 	fmt.Println(totalEmails)
-	fmt.Println(float64(counter) / float64(totalEmails))
+	fmt.Println(float64(numMisses) / float64(totalEmails))
 
 }
 

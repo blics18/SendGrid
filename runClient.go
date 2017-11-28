3223ba3e580325e1fbdbe29ce7c666b8c16b0726
BL MacBook <BLMac@Brendas-MacBook-Pro.local>
2017-11-27 17:41:14 -0800

    Populates and reads in from data.txt and parses it

--- runClient.go
+++ runClient.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-
+	"os"
+	"strings"
 	"github.com/blics18/SendGrid/client"
+	"strconv"
 )
 
 func main() {
@@ -14,11 +17,32 @@ func main() {
 	fmt.Println("Starting Client")
 
 	// populate the MySQL Database - numUsers, numEmails, numTables
-	db := client.PopulateDB(1, 1, 1)
+	db := client.PopulateDB(10, 5000, 5)
 	defer db.Close()
 
 	// populate the Bloom Filter from values in the MySQL Database
-	// client.Populate()
+	client.Populate()
+
+
+	fileHandle, _ := os.Open("data/data.txt")
+	defer fileHandle.Close()
+	fileScanner := bufio.NewScanner(fileHandle)
+
+	numMisses := 0
+	totalEmails := 0
+	
+	for fileScanner.Scan() {
+		s := strings.Split(fileScanner.Text(), ":")
+		id, emails := s[0], s[1]
+		userID, _ := strconv.Atoi(id)
+		userEmails := strings.Split(emails, " ")
+		_, resp := client.Check(userID, userEmails)
+		numMisses += resp.Hits
+		totalEmails += resp.Total
+		fmt.Println("Ratio: ", float64(resp.Hits) / float64(resp.Total))
+	}
+
+	fmt.Println("Total Ratio: ", float64(numMisses) / float64(totalEmails))
 
 	// to check if values are in the Bloom Filter. Note: Remember to replace the value of b and the userID in Check to what they are in the MySQL DB.
 	// b := []string{"NmNTsOQJOl@aol.com"}

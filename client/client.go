723d11e4c0e7c394f8fc70e91cc6685467dea386
BL MacBook <BLMac@Brendas-MacBook-Pro.local>
2017-11-27 17:38:14 -0800

    Response from checkBF is Hit/Miss Struct. Added write to file in Populate()

--- client/client.go
+++ client/client.go
@@ -6,7 +6,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"net/http"
+	"os"
+	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -31,20 +34,27 @@ type HealthStatus struct {
 	}
 }
 
-func Check(userID int, emails []string) (error, []byte) {
+type HitMiss struct {
+	Hits  int
+	Total int
+}
+
+func Check(userID int, emails []string) (error, HitMiss) {
 	user := User{
 		UserID: &userID,
 		Email:  emails,
 	}
 
+	var hitMissStruct HitMiss
+
 	userJSON, err := json.MarshalIndent(user, "", " ")
 	if err != nil {
-		return err, []byte("0")
+		return err, hitMissStruct
 	}
 
 	req, err := http.NewRequest("GET", "http://localhost:8082/checkBF", bytes.NewBuffer(userJSON))
 	if err != nil {
-		return err, []byte("0")
+		return err, hitMissStruct
 	}
 
 	req.Header.Set("Content-Type", "application/json")
@@ -53,18 +63,23 @@ func Check(userID int, emails []string) (error, []byte) {
 
 	resp, err := client.Do(req)
 	if err != nil {
-		return err, []byte("0")
+		return err, hitMissStruct
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return err, []byte("0")
+		return err, hitMissStruct
+	}
+
+	err = json.Unmarshal(body, &hitMissStruct)
+	if err != nil {
+		return err, hitMissStruct
 	}
 
 	fmt.Println("Response: ", string(body))
 
 	resp.Body.Close()
-	return nil, body
+	return nil, hitMissStruct
 }
 
 func Clear() error {
@@ -106,8 +121,8 @@ func Populate() error {
 		db.Close()
 	}
 
-	var tableNames []string            // tableNames is a list of tables. Example: [User00, User01, User03, ...]
-	userMap := make(map[*int][]string) // userMap is a map that consists of User ID's as keys, with their value as a list of emails. //Ex: [5: ["jim@yahoo.com", "trevor@aol.com"]
+	var tableNames []string           // tableNames is a list of tables. Example: [User00, User01, User03, ...]
+	userMap := make(map[int][]string) // userMap is a map that consists of User ID's as keys, with their value as a list of emails. //Ex: [5: ["jim@yahoo.com", "trevor@aol.com"]
 
 	stmt := fmt.Sprintf("SELECT TABLE_NAME AS tableName FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_SCHEMA='UserStructs'")
 	rows, err := db.Query(stmt)
@@ -137,17 +152,19 @@ func Populate() error {
 			var email string
 			rows.Scan(&id, &email)
 
-			_, exists := userMap[&id]
+			_, exists := userMap[id]
 
 			if exists {
-				userMap[&id] = append(userMap[&id], email)
+				userMap[id] = append(userMap[id], email)
 			} else {
-				userMap[&id] = []string{email}
+				userMap[id] = []string{email}
 			}
 		}
 
 		rows.Close()
 	}
+
+	writeDataToFile(userMap)
 
 	userList := make([]User, len(userMap)) // userList is a list of User structs: [User, User, User]
 	index := 0
@@ -155,7 +172,7 @@ func Populate() error {
 	// build userList from the values in userMap
 	for key, value := range userMap {
 		userList[index] = User{
-			UserID: key,
+			UserID: &key,
 			Email:  value,
 		}
 		index++
@@ -220,4 +237,21 @@ func HealthCheck() error {
 
 	resp.Body.Close()
 	return nil
+}
+
+func writeDataToFile(userMap map[int][]string) {
+	file, err := os.Create("data/data.txt")
+	if err != nil {
+		return
+	}
+
+	defer file.Close()
+
+	for key, value := range userMap {
+		emailsNotInBF := makeRandomEmails(rand.Intn(5) + 1)
+		dataString := fmt.Sprintf("%d:%s %s\n", key, strings.Join(value, " "), strings.Join(emailsNotInBF, " "))
+
+		file.WriteString(dataString)
+
+	}
 }

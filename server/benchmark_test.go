e6fe07be4a72b6841215e22a96758c5cd3d8e5a6
David Pham <dppham1@uci.edu>
2017-12-06 14:30:14 -0800

    Update benchmark_test.go

--- server/benchmark_test.go
+++ server/benchmark_test.go
@@ -16,12 +16,73 @@ import (
 )
 
 func BenchmarkClear(b *testing.B) {
+<<<<<<< HEAD
+	client.PopulateDB(10, 100, 5)
+	cfg := client.GetEnv()
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		client.Populate(cfg)
+		b.StopTimer()
+		client.Clear(cfg)
+		b.StartTimer()
+=======
 	for n := 0; n < b.N; n++ {
 		client.Clear()
+>>>>>>> master
 	}
 }
 
 func BenchmarkPopulate(b *testing.B) {
+<<<<<<< HEAD
+	client.PopulateDB(10, 100, 5)
+	cfg := client.GetEnv()
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		client.Clear(cfg)
+		b.StopTimer()
+		client.Populate(cfg)
+		b.StartTimer()
+	}
+	
+}
+
+func benchmarkCheck(i int, email []string, b *testing.B) {
+	db, _ := client.PopulateDB(10, 100, 5)
+	cfg := client.GetEnv()
+	client.Populate(cfg)
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		_, err := client.Check(cfg, i, email)
+		if err != nil{
+			b.Error(err)
+			b.FailNow()
+		}
+	}
+	// client.DropTables(5, db)
+	db.Close()
+}
+
+func BenchmarkCheck1(b *testing.B) {
+	users := client.MakeRandomUsers(1, 100)
+	for n := 0; n < len(users); n++ {
+		benchmarkCheck(*users[n].UserID, users[n].Email, b)
+	}
+}
+
+func BenchmarkCheck10(b *testing.B) {
+	users := client.MakeRandomUsers(10, 100)
+	for n := 0; n < len(users); n++ {
+		benchmarkCheck(*users[n].UserID, users[n].Email, b)
+	}
+}
+
+// func BenchmarkCheck1000(b *testing.B) {
+// 	users := client.MakeRandomUsers(1000, 100)
+// 	for n := 0; n < len(users); n++ {
+// 		benchmarkCheck(*users[n].UserID, users[n].Email, b)
+// 	}
+// }
+=======
 	for n := 0; n < b.N; n++ {
 		client.Populate()
 	}
@@ -32,3 +93,4 @@ func BenchmarkCheck(b *testing.B) {
 		client.Check()
 	}
 }
+>>>>>>> master
